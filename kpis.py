a735abc569bd133d6c863c12a1416fcce480932c
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-07-14 14:53:54 -0500

    Add comment re calculating median of total daily commits

--- kpis.py
+++ kpis.py
@@ -17,6 +17,8 @@ commits = pd.read_csv('kpis.csv', parse_dates=True, index_col='Date')
 # Calculate a column of total daily commits.
 commits['Total']= commits.ForteFoto + commits.Private + commits.Support + \
         commits.Jupyter + commits.Tableau + commits.KPI
+# Calculate a column of the median of the column of total daily commits.
+# One day fix this so that it's not an additional column, but a number to plot.
 commits['Median'] = commits['Total'].median()
 
 # Define the graph title and subtitle, and the x and y axis labels.
