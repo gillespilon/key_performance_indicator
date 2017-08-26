fe509898d8a49fdb7c246c9e52a7a022bb97fdd7
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-08-26 11:52:52 -0500

    Add HT1ST, HT2ST, HTPT

--- kpis.py
+++ kpis.py
@@ -17,7 +17,7 @@ commits = pd.read_csv('kpis.csv', parse_dates=True, index_col='Date')
 commits['Total']= commits.ForteF + commits.Private + commits.Support + \
         commits.Jupyter + commits.Tableau + commits.KPI + commits.MSHA + \
         commits.Anscombe + commits.Cholera + commits.ImpactEffort +\
-        commits.PSA
+        commits.PSA + commits.HT1ST + commits.HT2ST + commits.HTPT
 # Calculate a column of the median of the column of total daily commits.
 # One day fix this so that it's not an additional column, but a number to plot.
 commits['Median'] = commits['Total'].median()
