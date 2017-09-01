c61918a6ee757d1d23974d2e6ac3205d9e562e87
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-09-01 06:38:08 -0500

    Add "in brevi" + content

--- kpis.py
+++ kpis.py
@@ -2,6 +2,16 @@
 
 # Key Performance Indicators (KPIs)
 #
+# In brevi
+#
+# Office workers create, update, and publish documents. Whether it's
+# creating presentations, responding to emails, or doing statistical analysis,
+# they are creating documents. Unlike a manufacturing environment, their
+# output appears invisible to an observer of their work process. If such a
+# worker were to commit their documents to a Git repository, they could make
+# their work visible . These commits could then be reported as a key
+# performance indicator (KPI).
+
 # Import the required libraries and modules.
 import pandas as pd
 import matplotlib.pyplot as plt
