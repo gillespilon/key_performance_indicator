2b63638d780d9f25a6bfc8968db93f7cecf452de
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-10-01 16:57:55 -0500

    Convert .ipynb to .py

---
+++ kpi_invoice_preparation.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+invoicing = pd.read_csv('invoice_preparation_time.csv', parse_dates=True, index_col='Date')
+
+invoicing['Start invoice'] = pd.to_datetime(invoicing['Start invoice'], format='%H:%M')
+invoicing['Send invoice'] = pd.to_datetime(invoicing['Send invoice'], format='%H:%M')
+invoicing['total_time'] = (invoicing['Send invoice'] - invoicing['Start invoice'])
+invoicing['Total time'] = invoicing['total_time'] / np.timedelta64(1, 'm')
+invoicing = invoicing.drop(['total_time'], axis=1)
+
+title = 'Invoicing cycle time'
+subtitle = 'Start of invoice to invoice sent'
+ylabel = 'Time (min)'
+xlabel = 'Date'
+
+ax = invoicing['Total time'].plot.line(legend=False, marker='o', markersize=3)
+ax.axis('auto')
+for spine in 'right', 'top':
+    ax.spines[spine].set_color('none')
+ax.set_title(title + '\n' + subtitle)
+ax.set_ylabel(ylabel)
+ax.set_xlabel(xlabel)
+ax.figure.savefig('invoice_cycle_time.svg', format='svg')
+ax.figure.savefig('invoice_cycle_time.pdf', format='pdf')
+
