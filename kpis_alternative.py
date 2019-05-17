44c48c95ee5591bf0ec851bd1f3bed57a10cce2c
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-05-17 09:46:43 -0500

    Added sufficient code to complete this alternative approach.

--- kpis_alternative.py
+++ kpis_alternative.py
@@ -9,6 +9,15 @@ import subprocess
 from datetime import timezone
 import pandas as pd
 from matplotlib.dates import DateFormatter
+import matplotlib.axes as axes
+
+def despine(ax: axes.Axes) -> None:
+    '''
+    Remove the top and right spines of a graph.
+    '''
+    for spine in 'right', 'top':
+        ax.spines[spine].set_color('none')
+
 
 author_dates = subprocess.check_output(['git', 'log', '--pretty=%aI'],
                                         universal_newlines=True) \
@@ -19,5 +28,7 @@ s = pd.Series([pd.to_datetime(aI).astimezone(timezone.utc)
 print(s.head())
 print(s.describe())
 last_50 = s.nlargest(50).groupby(s.dt.date).agg('count')
-ax = last_50.plot(style='.', rot=45, figsize=(9,2))
+ax = last_50.plot.line(rot=45, figsize=(8,6))
 ax.get_xaxis().set_major_formatter(DateFormatter('%Y-%m-%d'))
+despine(ax)
+ax.figure.savefig('commits_alternative.svg', format='svg')
