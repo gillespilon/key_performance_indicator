15dea279e31073bbd754f26de01b84e9e94d05d0
Gilles Pilon <gilles.pilon@gillespilon.ca>
2019-06-17 11:02:55 -0500

    Added "def despine", import matplotlib axes

--- kpi_invoice_preparation.py
+++ kpi_invoice_preparation.py
@@ -12,6 +12,15 @@ time -f '%e' ./kpi_invoice_preparation.py
 
 import pandas as pd
 import numpy as np
+import matplotlib.axes as axes
+
+def despine(ax: axes.Axes) -> None:
+    '''
+    Remove the top and right spines of a graph.
+    '''
+    for spine in 'right', 'top':
+        ax.spines[spine].set_color('none')
+
 
 invoicing = pd.read_csv('invoice_preparation_time.csv',
                         parse_dates=True,
@@ -30,8 +39,7 @@ ylabel = 'Time (min)'
 xlabel = 'Date'
 ax = invoicing['Total time'].plot.line(legend=False, marker='o', markersize=3)
 ax.axis('auto')
-for spine in 'right', 'top':
-    ax.spines[spine].set_color('none')
+despine(ax)
 ax.set_title(title + '\n' + subtitle)
 ax.set_ylabel(ylabel)
 ax.set_xlabel(xlabel)
