d2855e2dfc601e575c6751a4383ec50144fdb55d
Gilles Pilon <gillespilon13@gmail.com>
2019-11-17 16:15:33 -0600

    Update docstring for def despine

--- kpis.py
+++ kpis.py
@@ -103,6 +103,9 @@ def recent_activity() -> pd.DataFrame:
 def despine(ax: axes.Axes) -> None:
     '''
     Remove the top and right spines of a graph.
+
+    Used to enforce standard and *correct* style. There is only one x, and one
+    y axis, left and bottom, therefore there should only be these axes.
     '''
     for spine in 'right', 'top':
         ax.spines[spine].set_visible(False)
