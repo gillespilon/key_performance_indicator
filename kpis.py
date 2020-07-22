gillespilon/key_performance_indicator

commit 467f9a970dfd35c681666d3b2a81f30f447548b6

Simplify abscissa with autofmt_xdate()

kpis.py
@@ -5,37 +5,30 @@ Calculate the number of daily commits for all git repositories.
 Draw a line plot of daily commits versus date.
 Must manually update the repository path list repositories.ods.
 
-    time -f '%e' ./kpis.py > kpis.txt
-    ./kpis.py > kpis.txt
-    ./kpis.py | tee kpis.txt
+time -f '%e' ./kpis.py > kpis.txt
+time -f '%e' ./kpis.py
+./kpis.py > kpis.txt
+./kpis.py
 '''
 
 # time -f '%e' ./kpis.py > kpis.txt
 # ./kpis.py > kpis.txt
 
 
-import subprocess
-from os import chdir
-from typing import List, Dict, Optional
-from pathlib import Path
 from datetime import date, datetime, timedelta
+from typing import List, Dict, Optional
 from itertools import groupby
+from pathlib import Path
+import subprocess
 
 from dateutil.parser import parse as parsedate
-import pandas as pd
-import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 import matplotlib.axes as axes
-from matplotlib.dates import DateFormatter, DayLocator
-from matplotlib.ticker import NullFormatter
-from matplotlib.ticker import NullLocator
-from matplotlib.ticker import MaxNLocator
+import matplotlib.cm as cm
+import pandas as pd
 
 
-# chdir(Path(__file__).parent.__str__())
-
-
-c = cm.Paired.colors  # c[0] c[1] ... c[11]
-# https://matplotlib.org/tutorials/colors/colormaps.html
+c = cm.Paired.colors
 
 
 def main():
@@ -139,14 +132,10 @@ def plot_recent_activity(activity: Optional[pd.DataFrame] = None) -> None:
     if activity is None:
         activity = recent_activity()
     commits = activity.reset_index().groupby('date').agg('sum')
-    ax = commits.plot(y='commits',
-                      legend=False,
-                      style='.-',
-                      color=c[0],
-                      rot=90,
-                      figsize=(12, 6),
-                      x_compat=True)
-    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(111)
+    fig.autofmt_xdate()
+    ax.plot(commits['commits'], color=c[0])
     print(f'Commits by date\n{commits}\n')
     print(f"Median commits: {commits['commits'].median().astype(int)}\n")
     print(f"Commits by ascending value\n{commits.sort_values(by='commits')}\n")
@@ -155,10 +144,6 @@ def plot_recent_activity(activity: Optional[pd.DataFrame] = None) -> None:
     ax.plot(commits['low'], marker='x', color=c[5])
     ax.set_ylabel(ylabel)
     ax.set_xlabel(xlabel)
-    ax.xaxis.set_minor_locator(NullLocator())
-    ax.xaxis.set_major_locator(DayLocator())
-    ax.xaxis.set_minor_formatter(NullFormatter())
-    ax.xaxis.set_major_formatter(DateFormatter('%d'))
     ax.set_title(title, fontweight='bold')
     ax.autoscale(tight=False)
     ax.axhline(y=commits['commits'].median(), color=c[1])

commit 455d62eff91b471012c5f7236b48de4cfaa6de55

Fix running under cron or in $HOME again
The chdir to this script's dir, where repositories.ods is located, was
commented, then deleted, and no traceback was ever preserved of the real
original problem.

    Traceback (most recent call last):
      File "/home/gilles/documents/websites/support.gillespilon.ca/fil/key_performance_indicator/kpis.py", line 151, in <module>
        main()
      File "/home/gilles/documents/websites/support.gillespilon.ca/fil/key_performance_indicator/kpis.py", line 31, in main
        activity = recent_activity()
      File "/home/gilles/documents/websites/support.gillespilon.ca/fil/key_performance_indicator/kpis.py", line 97, in recent_activity
        paths = repository_paths()
      File "/home/gilles/documents/websites/support.gillespilon.ca/fil/key_performance_indicator/kpis.py", line 67, in repository_paths
        in pd.read_excel('repositories.ods',)
      File "/usr/lib/python3.8/site-packages/pandas/io/excel/_base.py", line 304, in read_excel
        io = ExcelFile(io, engine=engine)
      File "/usr/lib/python3.8/site-packages/pandas/io/excel/_base.py", line 824, in __init__
        self._reader = self._engines[engine](self._io)
      File "/usr/lib/python3.8/site-packages/pandas/io/excel/_odfreader.py", line 23, in __init__
        super().__init__(filepath_or_buffer)
      File "/usr/lib/python3.8/site-packages/pandas/io/excel/_base.py", line 353, in __init__
        self.book = self.load_workbook(filepath_or_buffer)
      File "/usr/lib/python3.8/site-packages/pandas/io/excel/_odfreader.py", line 34, in load_workbook
        return load(filepath_or_buffer)
      File "/usr/lib/python3.8/site-packages/odf/opendocument.py", line 982, in load
        z = zipfile.ZipFile(odffile)
      File "/usr/lib/python3.8/zipfile.py", line 1250, in __init__
        self.fp = io.open(file, filemode)
    FileNotFoundError: [Errno 2] No such file or directory: 'repositories.ods'

There probably wasn't really a problem in the first places.

Works now.

Fixes: 467f9a9 (Simplify abscissa with autofmt_xdate())

kpis.py
@@ -15,6 +15,7 @@ from datetime import date, datetime, timedelta
 from typing import List, Dict, Optional
 from itertools import groupby
 from pathlib import Path
+from os import chdir
 import subprocess
 
 from dateutil.parser import parse as parsedate
@@ -22,6 +23,9 @@ import matplotlib.pyplot as plt
 import matplotlib.axes as axes
 import matplotlib.cm as cm
 import pandas as pd
+
+
+chdir(Path(__file__).parent.__str__())
 
 
 c = cm.Paired.colors