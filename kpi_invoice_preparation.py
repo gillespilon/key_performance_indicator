29af61ae88c446626b1c158c07f7f61a7053f80e
Gilles Pilon <gillespilon13@gmail.com>
2020-09-30 10:38:47 -0500

    Replace def despine() docstring

--- kpi_invoice_preparation.py
+++ kpi_invoice_preparation.py
@@ -22,11 +22,17 @@ columns = ['Start invoice', 'Send invoice', 'Total time']
 
 
 def despine(ax: axes.Axes) -> None:
-    '''
+    """
     Remove the top and right spines of a graph.
 
-    There is only one x axis, on the bottom, and one y axis, on the left.
-    '''
+    Parameters
+    ----------
+    ax : axes.Axes
+
+    Example
+    -------
+    >>> despine(ax)
+    """
     for spine in 'right', 'top':
         ax.spines[spine].set_visible(False)
 
