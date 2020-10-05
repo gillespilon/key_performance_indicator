a23791eb6fdb7ff89ed32b8f3b852553b2c8b39b
Gilles Pilon <gillespilon13@gmail.com>
2020-10-05 14:44:24 -0500

    Refactor all code

--- kpi_invoice_preparation.py
+++ kpi_invoice_preparation.py
@@ -1,42 +1,50 @@
 #! /usr/bin/env python3
-'''
+"""
 Calculate invoice preparation time
 
 ./kpi_invoice_preparation.py
 time -f '%e' ./kpi_invoice_preparation.py
-'''
-
-from typing import List
+"""
 
 import matplotlib.axes as axes
 import datasense as ds
 import pandas as pd
 import numpy as np
 
-title = 'Invoicing cycle time'
-subtitle = 'Start of invoice to invoice sent'
+fig_title = 'Invoicing cycle time'
+ax_title = 'Start of invoice to invoice sent'
 ylabel = 'Time (min)'
-xlabel = 'Date'
-columns = ['Start invoice', 'Send invoice', 'Total time']
+date_column = 'Date'
+invoice_start_column = 'Start invoice'
+invoice_send_column = 'Send invoice'
+total_time_column = 'Total time'
+data_file_name = 'invoice_preparation_time.csv'
 
 
 def main():
     invoicing = pd.read_csv(
-        'invoice_preparation_time.csv',
-        parse_dates=['Date']
+        data_file_name,
+        parse_dates=[date_column]
     )
-    print(invoicing.head())
-    print(invoicing.dtypes)
-    df = calculate_cycle_time(
-        invoicing,
-        columns
+    invoicing = calculate_cycle_time(
+        df=invoicing,
+        date_column=date_column,
+        invoice_start_column=invoice_start_column,
+        invoice_send_column=invoice_send_column,
+        total_time_column=total_time_column
     )
-    print(df.head())
-    print(df.dtypes)
-    plot_cycle_time(
-        invoicing,
-        column_x='Date',
-        column_y='Total time'
+    fig, ax = ds.plot_line_x_y(
+        X=invoicing[date_column],
+        y=invoicing[total_time_column]
+    )
+    despine(ax)
+    fig.suptitle(fig_title)
+    ax.set_title(ax_title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(date_column)
+    fig.savefig(
+        'invoice_cycle_time.svg',
+        format='svg'
     )
 
 
@@ -58,41 +66,30 @@ def despine(ax: axes.Axes) -> None:
 
 def calculate_cycle_time(
         df: pd.DataFrame,
-        columns: List[str]
+        date_column: str,
+        invoice_start_column: str,
+        invoice_send_column: str,
+        total_time_column: str
         ) -> pd.DataFrame:
-    '''
+    """
     Calculate cycle time with two columns, store in third column
-    columns[0] == start time
-    columns[1] == end time
-    columns[2] == total time
-    '''
-    df[columns[0]] = pd.to_datetime(
-        df[columns[0]],
+    """
+    df[invoice_start_column] = pd.to_datetime(
+        df[invoice_start_column],
         format='%H:%M'
     )
-    df[columns[1]] = pd.to_datetime(
-        df[columns[1]],
+    df[invoice_send_column] = pd.to_datetime(
+        df[invoice_send_column],
         format='%H:%M'
     )
-    df[columns[2]] = (df[columns[1]] - df[columns[0]])
-    df[columns[2]] = df[columns[2]] / np.timedelta64(1, 'm')
+    df[total_time_column] = (
+        df[invoice_send_column] - df[invoice_start_column]
+    )
+    # This creates a timedelta column of minutes
+    df[total_time_column] = df[total_time_column] / np.timedelta64(1, 'm')
+    # This creates an integer column of minutes
+    # df[total_time_column] = df[total_time_column].dt.components['minutes']
     return df
-
-
-def plot_cycle_time(
-        df: pd.DataFrame,
-        column_x: str,
-        column_y: str
-        ) -> None:
-    fig, ax = ds.plot_line_x_y(
-        X=df[column_x],
-        y=df[column_y]
-    )
-    despine(ax)
-    ax.set_title(title + '\n' + subtitle)
-    ax.set_ylabel(ylabel)
-    ax.set_xlabel(xlabel)
-    fig.savefig('invoice_cycle_time.svg', format='svg')
 
 
 if __name__ == '__main__':
